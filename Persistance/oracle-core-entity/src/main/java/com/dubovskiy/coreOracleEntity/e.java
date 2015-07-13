b524d3df32d3068ee76164e2b06d1c42b54c16fa
Misha <mikhail.dubovskiy@outlook.com>
2015-07-13 23:07:34 +0300

    - rename module

---
+++ Persistance/oracle-core-entity/src/main/java/com/dubovskiy/coreOracleEntity/e.java
@@ -0,0 +1,7 @@
+package com.dubovskiy.coreOracleEntity;
+
+/**
+ * Created by Misha on 13.07.2015.
+ */
+public class e {
+}
--- Persistance/oracle-hibernate/src/main/java/com/dubovskiy/dao/Dao.java
+++ Persistance/oracle-hibernate/src/main/java/com/dubovskiy/dao/Dao.java
@@ -5,4 +5,6 @@ package com.dubovskiy.dao;
  */
 public interface Dao {
 
+   /*  findAll();*/
+
 }
