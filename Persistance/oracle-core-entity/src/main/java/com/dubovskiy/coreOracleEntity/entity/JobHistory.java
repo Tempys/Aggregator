444d2842f2efbe60e884949c8985b90f50f7e2c7
Misha <mikhail.dubovskiy@outlook.com>
2015-07-20 22:52:00 +0300

    - add JobHistory Entity

--- Persistance/oracle-core-entity/src/main/java/com/dubovskiy/coreOracleEntity/entity/JobHistory.java
+++ Persistance/oracle-core-entity/src/main/java/com/dubovskiy/coreOracleEntity/entity/JobHistory.java
@@ -1,12 +1,12 @@
 package com.dubovskiy.coreOracleEntity.entity;
 
 import javax.persistence.*;
+import java.sql.Timestamp;
 
 /**
  * Created by Misha on 19.07.2015.
  */
 @Entity(name = "JOB_HISTORY")
-@IdClass(JobHistoryPk.class)
 public class JobHistory {
 
     @EmbeddedId
@@ -14,6 +14,21 @@ public class JobHistory {
             @AttributeOverride(name = "employeeId", column = @Column(name = "EMPLOYEE_ID", nullable = false, precision = 6, scale = 0)),
             @AttributeOverride(name = "startDate", column = @Column(name = "START_DATE", nullable = false, columnDefinition = "TIMESTAMP"))})
     private JobHistoryPk jobHistoryPk;
+    private Timestamp endDate;
 
+    public JobHistoryPk getJobHistoryPk() {
+        return jobHistoryPk;
+    }
 
+    public void setJobHistoryPk(JobHistoryPk jobHistoryPk) {
+        this.jobHistoryPk = jobHistoryPk;
+    }
+
+    public Timestamp getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Timestamp endDate) {
+        this.endDate = endDate;
+    }
 }
